ea021b5292c66b14bf4253176e1e8b51298bf9ad
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-28 15:45:04 +0530

    drawn landmark of face

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import time
 cap = cv2.VideoCapture(0)
 p_time = 0
 
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=2)
+mp_draw = mp.solutions.drawing_utils
+
 
 while True:
     success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = face_mesh.process(img_rgb)
+    print(result.multi_face_landmarks)
+    if result.multi_face_landmarks:
+        for face_lm in result.multi_face_landmarks:
+            mp_draw.draw_landmarks(img, face_lm)
 
     c_time = time.time()
     fps = 1 / (c_time - p_time)
     p_time = c_time
     cv2.putText(
-        img, str(int(fps)),
+        img, f'FPS: {int(fps)}',
         (10, 60), cv2.FONT_HERSHEY_PLAIN,
-        3, (255, 0, 255), 3
+        2, (255, 0, 255), 2
     )
 
     cv2.imshow("Image", img)
